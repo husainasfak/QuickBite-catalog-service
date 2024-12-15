9b317baaed9f27088e57d61aaee16bb99df37dc4
Ashfaq Hussain <asfak@vcbay.co>
2024-12-15 16:18:52 +0530

    add config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
+import config from "config";
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.json({
+        msg: config.get("server.port"),
+    });
 });
 
 app.use(globalErrorHandler);
